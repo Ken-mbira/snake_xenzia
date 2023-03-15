fffb3717c2d67806b00e1ac43e549069258162a0
Ken Mbira <devmbira@gmail.com>
2023-03-15 13:35:47 +0300

    set up docker and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 const path = require('path');
+const htmlwebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    mode : 'development',
-    entry : './src/index.js',
-    output : {
-        filename : 'main.js',
-        path : path.resolve(__dirname, 'dist'),
-    },
-    module : {
-        rules : [
-            {
-                test : /\.css$/i,
-                use : ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    devServer : {
-        static : './dist'
+module.exports = (env) => {
+    return {
+        mode : env.environment,
+        entry : './src/index.js',
+        output : {
+            filename : 'main.js',
+            path : path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+        module : {
+            rules : [
+                {
+                    test : /\.css$/i,
+                    use : ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        plugins: [
+            new htmlwebpackPlugin({
+                title: "Snake Xenzia"
+            })
+        ],
+        devServer : {
+            static : './dist',
+            compress : true,
+            port : 3000
+        }
     }
 }
